1124e4e6ede898205e4a213d4466e764002aebf3
mtoan12 <tranminhtoan1280@gmail.com>
2023-12-03 09:46:45 +0700

    social login

--- frontend/src/components/social-login/SocialLogins.tsx
+++ frontend/src/components/social-login/SocialLogins.tsx
@@ -1,25 +1,17 @@
-import { useGoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { Button } from '../ui/button';
 
 const SocialLogins = () => {
   const googleLogin = useGoogleLogin({
     onSuccess: (tokenResponse) => console.log(tokenResponse),
-    scope: ['profile', 'email'].join(' '),
+    scope: 'profile',
     onError: (error) => console.log(error)
   });
   return (
     <section className='flex flex-col items-center justify-center '>
       <h2 className='mb-4 text-2xl font-bold'>Or continue with</h2>
       <div className='flex items-center space-x-4'>
-        <Button
-          className='flex items-center space-x-2'
-          onClick={() => {
-            googleLogin();
-          }}
-        >
-          <img src='https://img.icons8.com/color/48/000000/google-logo.png' alt='google' className='h-6 w-6' />
-          <span>Google</span>
-        </Button>
+        <GoogleLogin onSuccess={(token) => console.log(token)} />
         <Button className='flex items-center space-x-2' onClick={() => {}}>
           <img src='https://img.icons8.com/color/50/000000/facebook.png' alt='facebook' className='h-6 w-6' />
           <span>Facebook</span>
