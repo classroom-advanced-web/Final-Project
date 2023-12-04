33700e7184c9aaa47365adbe2e8f7f8c3cc690a3
mtoan12 <tranminhtoan1280@gmail.com>
2023-12-04 22:09:37 +0700

    sửa hàm formatDate

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,13 +1,13 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function formatDate(date: Date) {
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = (date.getDate() + 1).toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
 
-  return date.getFullYear() + "-" + month + "-" + day;
+  return date.getFullYear() + '-' + month + '-' + day;
 }
