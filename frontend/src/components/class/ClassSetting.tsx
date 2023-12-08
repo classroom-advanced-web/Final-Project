5fba8624ad3db3a8e4c79e3f5d340d4f18c967d5
mtoan12 <20120215@student.hcmus.edu.vn>
2023-12-08 19:02:26 +0700

    setting class

--- frontend/src/components/class/ClassSetting.tsx
+++ frontend/src/components/class/ClassSetting.tsx
@@ -1,11 +1,35 @@
-import { IoSettingsOutline } from "react-icons/io5";
-import { Button } from "../ui/button";
+import { IoSettingsOutline } from 'react-icons/io5';
+import { Button } from '../ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog';
 
 const ClassSetting = () => {
   return (
-    <Button variant='ghost' className='text-xl'>
-      <IoSettingsOutline />
-    </Button>
+    <div>
+      {/* <Button variant='ghost' className='text-xl'>
+        <IoSettingsOutline />
+      </Button> */}
+      <Dialog>
+        <DialogTrigger>
+          <IoSettingsOutline />
+        </DialogTrigger>
+        <DialogContent className='max-w-screen inset-0 h-screen w-screen translate-x-0 translate-y-0'>
+          <DialogHeader>
+            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our
+              servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
 export default ClassSetting;
