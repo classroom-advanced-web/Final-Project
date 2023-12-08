28df4d45ec9b2561d5a685a56c47e701d9e54eca
duytruong-23 <zduytruongz@gmail.com>
2023-12-08 16:39:16 +0700

    Fix cors

--- backend/src/main/java/com/example/backend/configurations/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/configurations/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("*");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
+//        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new
                 UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
