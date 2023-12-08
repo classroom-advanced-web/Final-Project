f559c63d3c01c9d1c6ec3a5fd43256f48f964037
mtoan12 <20120215@student.hcmus.edu.vn>
2023-12-08 12:14:11 +0700

    class card

--- frontend/src/constance/constance.ts
+++ frontend/src/constance/constance.ts
@@ -1,1 +1,14 @@
 export const UNDEFINED_ERROR = 'Undefined error';
+export const thumbs = [
+  'https://gstatic.com/classroom/themes/img_breakfast_thumb.jpg',
+  'https://gstatic.com/classroom/themes/Honors_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_graduation_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_bookclub_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_code_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_reachout_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_learnlanguage_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_backtoschool_thumb.jpg',
+  'https://gstatic.com/classroom/themes/img_read_thumb.jpg'
+];
+
+export const DEFAULT_THUMB = 'https://gstatic.com/classroom/themes/img_backtoschool_thumb.jpg';
