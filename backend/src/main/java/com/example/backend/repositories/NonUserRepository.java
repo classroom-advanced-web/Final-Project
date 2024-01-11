73c0a232be079366da3e978ec12d4ffed0c2d77f
duytruong-23 <zduytruongz@gmail.com>
2024-01-11 15:13:00 +0700

    save non user

--- backend/src/main/java/com/example/backend/repositories/NonUserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/NonUserRepository.java
@@ -4,7 +4,10 @@ import com.example.backend.entities.NonUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NonUserRepository extends JpaRepository<NonUser, String> {
     List<NonUser> findByClassroomId(String classroomId);
+
+    Optional<NonUser> findByStudentId(String studentId);
 }
