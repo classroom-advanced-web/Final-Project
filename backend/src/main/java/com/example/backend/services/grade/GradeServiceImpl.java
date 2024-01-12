df67fdf9d01be44398cda906f1f1ca168e612e30
duytruong-23 <zduytruongz@gmail.com>
2024-01-12 14:59:09 +0700

    check student id != null grade board

--- backend/src/main/java/com/example/backend/services/grade/GradeServiceImpl.java
+++ backend/src/main/java/com/example/backend/services/grade/GradeServiceImpl.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -70,14 +71,20 @@ public class GradeServiceImpl implements IGradeService{
     public List<StudentGradesDTO> getGradeBoard(String classroomId) {
         List<GradeComposition> gradeCompositions = gradeCompositionRepository.findByClassroomId(classroomId);
         List<StudentsClassroomRequestDTO> studentsClassroomRequestDTOs = classroomService.getStudentsOfClassroom(classroomId);
-        List<StudentGradesDTO> result = new ArrayList<>();
+        List<String> StudentIds = new ArrayList<>();
         for(StudentsClassroomRequestDTO studentsClassroomRequestDTO : studentsClassroomRequestDTOs) {
+            if(studentsClassroomRequestDTO.getStudentId() != null) {
+                StudentIds.add(studentsClassroomRequestDTO.getStudentId());
+            }
+        }
+        List<StudentGradesDTO> result = new ArrayList<>();
+        for(String studentId : StudentIds) {
             List<GradeDTO> gradeDTOs = new ArrayList<>();
             StudentGradesDTO studentGradesDTO = StudentGradesDTO.builder()
-                    .studentId(studentsClassroomRequestDTO.getStudentId())
+                    .studentId(studentId)
                     .build();
             for(GradeComposition gradeComposition : gradeCompositions) {
-                Grade grade = gradeRepository.findByStudentIdAndGradeCompositionId(studentsClassroomRequestDTO.getStudentId(), gradeComposition.getId())
+                Grade grade = gradeRepository.findByStudentIdAndGradeCompositionId(studentId, gradeComposition.getId())
                         .orElse(null);
 
                 if(grade != null) {
@@ -86,7 +93,7 @@ public class GradeServiceImpl implements IGradeService{
                     gradeDTOs.add(GradeDTO.builder()
                             .value(0.0)
                             .gradeComposition(gradeCompositionMapper.toDTO(gradeComposition))
-                            .studentId(studentsClassroomRequestDTO.getStudentId())
+                            .studentId(studentId)
                             .build());
                 }
             }
